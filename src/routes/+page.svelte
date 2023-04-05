090fc9dd73d93d9023e398e407aeef3cc614d694
austinwritescode <austlee@gmail.com>
2023-04-05 16:02:19 -0700

    Minor update to home screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,14 +10,20 @@
 
 <div class="p-4 flex flex-col items-center gap-1">
 	<img src="/apple-touch-icon.png" class="mask mask-squircle" height="180" width="180" />
-	<p class="text-4xl">Welcome to opentrail.org</p>
-	<p class="text-md mb-8 opacity-70">A free and open trail info community</p>
+	<p class="text-4xl">OpenTrail.org</p>
+	<p class="text-2xl">A free trail info community</p>
+	<p class="text-md mb-6 opacity-70">
+		Data contributions are open for public use under the
+		<a class="link" href="https://opendatacommons.org/licenses/odbl/summary/">
+			Open Database License
+		</a>
+	</p>
 	{#if $settings.trail !== '' && $settings.offline}
 		<p class="text-2xl">You have enabled offline mode for this trail:</p>
 	{:else}
 		<p class="text-2xl">To begin, select a trail</p>
 	{/if}
-	<p class="text-md mb-4 opacity-70">(This can be changed at any time under settings)</p>
+	<p class="text-md mb-4 opacity-70">(This can be changed later in the settings)</p>
 	<div class="flex gap-4">
 		{#each Object.keys(TRAILS) as trail}
 			{#if trail !== 'test'}

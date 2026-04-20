ccae5ab6ee48bc0994ed055ea6cf2a95a7f26955
austinwritescode <austlee@gmail.com>
2026-04-20 15:20:58 -0700

    Minor

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,14 @@
+import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
-import { exec } from 'child_process'
-import { promisify } from 'util'
 
 // Get current tag/commit and last commit date from
 const version = JSON.stringify(process.env.APP_VERSION || 'unknown');
 const lastmod = JSON.stringify(process.env.APP_LASTMOD || 'unknown');
 
 export default defineConfig({
+	plugins: [sveltekit()],
 	define: {
 		__VERSION__: version,
 		__LASTMOD__: lastmod,
 	}
 });
-
-const config: UserConfig = {
-	plugins: [sveltekit()],
-	define: {
-		__VERSION__: version,
-		__LASTMOD__: lastmod,
-	},
-};
-
-export default config;
